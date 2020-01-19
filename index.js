b82387df02ec36224f6b939849f0ef13fbe76075
Balbader <15971200+Balbader@users.noreply.github.com>
2020-01-19 23:35:10 +0100

    added code to html, css and js files

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+//document.querySelector('.button').addEventListener('click', (evt) => {
+    //alert('Boat was clicked!');
+//});
+
+
+document.querySelectorAll('.boat').forEach((elem) => {
+    elem.addEventListener('click', () => {
+        alert('It Works!')
+    });
+});
